15717bcc684867db52c1ee510f1623ad88a8787f
Justin Maronde <myemailisjustin@me.com>
2024-08-23 08:41:17 -0400

    Initial Commit

---
+++ LearnApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnApp
+//
+//  Created by Justin Maronde on 8/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnApp/LearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAppApp.swift
+//  LearnApp
+//
+//  Created by Justin Maronde on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
