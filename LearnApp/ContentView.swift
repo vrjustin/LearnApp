af284058c59e569e79811e852445ad40c33bd613
Justin Maronde <myemailisjustin@me.com>
2024-08-23 10:12:51 -0400

    make the list searchable and populate via the filteredFriends var if search is not empty else use the friends var.

--- LearnApp/ContentView.swift
+++ LearnApp/ContentView.swift
@@ -9,26 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var name: String = ""
-    @State private var friends: [String] = []
+    @State private var search: String = ""
+    @State private var friends: [String] = ["John", "Paul", "Ringo", "Dave", "Rick"]
+    @State private var filteredFriends: [String] = []
         
     var body: some View {
         VStack {
-            TextField("Enter Name: ", text: $name)
-                .textFieldStyle(.roundedBorder)
-                .onSubmit {
-                    friends.append(name)
-                    name = ""
-                }
-            List(friends, id: \.self) { friend in
+            List(!search.isEmpty ? filteredFriends : friends, id: \.self) { friend in
                 Text(friend)
             }
+            .listStyle(.plain)
+            .searchable(text: $search)
+            .onChange(of: search) {
+                filteredFriends = friends.filter {
+                    $0.contains(search)
+                }
+            }
+            
             Spacer()
         }
         .padding()
+        .onAppear(perform: {
+            filteredFriends = friends
+        })
+        .navigationTitle("Friends")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
--- LearnApp/LearnAppApp.swift
+++ LearnApp/LearnAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct LearnAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
