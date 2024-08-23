c722873d24198d54042ad6f70fa9c7f8ea07c363
Justin Maronde <myemailisjustin@me.com>
2024-08-23 08:50:47 -0400

    making the count var have the @State binding will allow the ContentView struct to be mutated. The only view that will get rerendered is the one that uses the state variable count. This is the Text view that uses it. ONLY the text view will be re-rendered upon changing the count value.

--- LearnApp/ContentView.swift
+++ LearnApp/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(count)")
+                .font(.title)
+            Button("Increment") {
+                count += 1
+            }
+            Button("Decrement") {
+                count -= 1
+            }
+
         }
         .padding()
     }
